0b85d40fb4c52d878644eff50a5c2c5518dea4fc
MaheshRamRamakrishnan <mahesharmy36@gmail.com>
2023-04-24 22:23:28 +0530

    fits commit

--- vehicle/wsgi.py
+++ vehicle/wsgi.py
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+=======
+"""
+WSGI config for vehicle project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -5,4 +15,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicle.settings')
 
 application = get_wsgi_application()
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/main
 app = application
